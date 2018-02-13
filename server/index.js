7d66635e7e4ad35d4c86bf7b23a5863335f6b295
Gabriella DiVentura <gabrielladiv@gmail.com>
2018-02-13 13:01:10 -0600

    complete

--- server/index.js
+++ server/index.js
@@ -1,17 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+const session = require('express-session');
+require('dotenv').config();
+
+const createInitialSession = require(`${__dirname}/middleswares/session.js`);
+const filter = require( `${__dirname}/middleswares/filter.js`);
 
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../build` ) );
 
+app.use( session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 10000 }
+  }));
+
+
+app.use((req, res, next) => createInitialSession(req,res,next));
+
+app.use( ( req, res, next ) => {
+    const { method } = req;
+    if ( method === "POST" || method === "PUT" ) {
+      filter( req, res, next );
+    } else {
+      next();
+    }
+  });
+  
+  
+
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
 app.put( `${messagesBaseUrl}`, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
+app.get(`${messagesBaseUrl}/history`, mc.history );
 
 const port = process.env.PORT || 3000
 app.listen( port, () => { console.log(`Server listening on port ${port}.`); } );
